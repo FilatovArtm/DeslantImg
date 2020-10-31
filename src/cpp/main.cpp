4131a8d434e25cd030cfc78917bc6d472fd95dc4
Artem Filatov <filatovartm@gmail.com>
2020-10-31 20:01:55 +0300

    add cli

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -7,27 +7,22 @@
 #include <iostream>
 
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-	// load input image
-	const cv::Mat img1 = cv::imread("data/test1.png", cv::IMREAD_GRAYSCALE);
-	const cv::Mat img2 = cv::imread("data/test2.png", cv::IMREAD_GRAYSCALE);
+	const cv::Mat img = cv::imread(argv[0], cv::IMREAD_GRAYSCALE);
 	
 	
 #ifdef USE_GPU 
 	// deslant on GPU
 	htr::CLWrapper clWrapper; // setup OpenCL, the same instance should be used for all following calls to deslantImg
-	const cv::Mat res1 = htr::deslantImg(img1, 255, clWrapper);
-	const cv::Mat res2 = htr::deslantImg(img2, 255, clWrapper);
+	const cv::Mat res1 = htr::deslantImg(img, 255, clWrapper);
 #else
 	// deslant on CPU
-	cv::Mat res1 = htr::deslantImg(img1, 255);
-	cv::Mat res2 = htr::deslantImg(img2, 255);
+	cv::Mat res1 = htr::deslantImg(img, 255);
 #endif
 
 	// write results to file
-	cv::imwrite("out1.png", res1);
-	cv::imwrite("out2.png", res2);
+	cv::imwrite(argv[1], res1);
 
 	return 0;
 }
